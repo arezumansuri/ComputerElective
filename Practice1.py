fc32c5378721d88d548300d60b39cf7de8555877
arezumansuri <15amansuri@iaa.edu.ec>
2015-05-08 08:23:02 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,14 @@
+name = 'Arezu'
+lastname = 'Mansuri'
+address = 'Los Ceibos, calle segunda, casa 117'
+email = '15amansuri@iaa.edu.ec'
+phonenumber = 593988911435
+
+print name 
+print lastname 
+print address 
+print email
+print phonenumber
+
+print "My full name is," name, lastname, "I live in," address, "To contact me my email is,", email, phonenumber,
+
