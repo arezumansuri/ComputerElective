5e9bf30441e7ffb54bd79d32a6d507c8e5c28662
arezumansuri <15amansuri@iaa.edu.ec>
2015-05-08 08:24:14 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,13 @@
+operand1 = 95
+operand2 = 64.5
+
+print operand1
+print operand2
+
+print "95 + 64.5 =" , operand1 + operand2 
+print "95 - 64.5 =" , operand1 - operand2
+print "95 * 64.5 =" , operand1 * operand2
+print "95 / 64.5 =" , operand1 / operand2
+print "95 % 64.5 =" , operand1 % operand2
+
+#i printed out the numbers 95 and 64.5 in order to make several mathematical operations
