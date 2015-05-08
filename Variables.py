f2e1042f023ee32ddbf509aa5e0d2dadff929d95
arezumansuri <15amansuri@iaa.edu.ec>
2015-05-08 08:23:23 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,21 @@
+name = 'Arezu Mansuri'
+age = 17
+gpa = 3.89
+
+print name
+print age
+print gpa
+
+print 'My Name is', name, '. I am ' , age , 'years old.' 
+
+print 'In ten years, I will be' , age + 10
+
+#Dog years
+ageInHumanYears = 3
+ageInDogYears = ageInHumanYears * 7
+print 'My dog is' , ageInDogYears, 'years old.' 
+
+#Order of Operations
+#Parenthesis, Exponents, Multiplication, Division, Addition, Substraction
+myNumber 5 + 4 * 5
+print myNumber
