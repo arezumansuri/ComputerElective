a2dff5883332c1ef7d615aacbadd80e1664ad1d3
arezumansuri <15amansuri@iaa.edu.ec>
2015-05-08 08:20:05 -0500

    Create HelloWorld_1.py

---
+++ HelloWorld_1.py
@@ -0,0 +1,15 @@
+print "Hello World"
+print "Welcome to programming for absolute beginners! \nThis is python."
+print 2015 #printing an integer
+print "2015" #printing a string
+print 3.1416 #printing a floating point number 
+
+#Expressions
+print 2 + 2
+print 2 - 2
+print 2 * 2
+print 2 / 2
+print 3 % 2
+
+print 3.1416 * 2.0
+print 3.1416 * 2
